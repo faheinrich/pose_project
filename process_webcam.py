4c9943e76322867c15f1525dbf102d6f00208645
faheinrich <faheinrich98@gmail.com>
2021-05-12 20:53:28 +0200

    Added detectron2 with model as pose estimator

--- process_webcam.py
+++ process_webcam.py
@@ -4,96 +4,95 @@ import cv2
 import numpy as np
 import time
 
-"""
-Simple script to capture, process and display wecam image
-"""
+# import tensorflow as tf
+import torch
+
+
+import detectron2
+
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+
+
+# print("Tensorflow: Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+# if torch.cuda.is_available():
+#     print("Torch: num available:", torch.cuda.device_count(), torch.cuda.get_device_name(0))
+#     dev = "cuda:0" 
+# else:
+#     print("torch got no gpu")
+#     dev = "cpu"  
+# device = torch.device(dev)  
 
 
 
 
-# def process_frame_with_cv2_net(frame):
 
-#     """
-#     https://learnopencv.com/deep-learning-based-human-pose-estimation-using-opencv-cpp-python/
-#     """
+# model_yaml = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+# model_yaml = "COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml"
+# model_yaml = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml"
 
-#     # Specify the paths for the 2 files
-#     protoFile = "/home/fabian/ros/cv2_net/pose/mpi/pose_deploy_linevec.prototxt"
-#     weightsFile = "/home/fabian/ros/cv2_net/pose/mpi/pose_iter_160000.caffemodel"
-#     # Read the network into Memory
-#     net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+model_yaml = "COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"
 
-#     inWidth = 1000
-#     inHeight = 1000
+score_threshold = 0.7
 
-#     threshold = 0.6
+cfg = get_cfg()   # get a fresh new config
+cfg.merge_from_file(model_zoo.get_config_file(model_yaml))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = score_threshold  # set threshold for this model
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_yaml)
+predictor = DefaultPredictor(cfg)
 
 
-#     inHeight = frame.shape[0]
-#     inWidth = frame.shape[1]
 
-
-#     # frame = cv2.resize(frame, (inHeight, inWidth))
-#     # Prepare the frame to be fed to the network
-#     inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight), (0, 0, 0), swapRB=False, crop=False)
-#     # Set the prepared object as the input blob of the network
-#     net.setInput(inpBlob)
-
-#     output = net.forward()
-
-
-#     H = output.shape[2]
-#     W = output.shape[3]
-
-#     # Empty list to store the detected keypoints
-#     points = []
-#     # 44 for mpi
-#     for i in range(44):
-#         # confidence map of corresponding body's part.
-#         probMap = output[0, i, :, :]
-
-#         # Find global maxima of the probMap.
-#         minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
-
-#         # Scale the point to fit on the original image
-#         x = (inWidth * point[0]) / W
-#         y = (inHeight * point[1]) / H
-#         if prob > threshold :
-#             cv2.circle(frame, (int(x), int(y)), 5, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-#             # cv2.putText(frame, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1.4, (0, 0, 255), 3, lineType=cv2.LINE_AA)
-#             # Add the point to the list if the probability is greater than the threshold
-#             points.append((int(x), int(y)))
-#         else :
-#             points.append(None)
-
-#     return frame
 
 
 def process_frame(frame):
-    start_time = time.time()
 
-    frame = frame[:,::-1,::-1]
 
-    print("Processing time in sec:", "%.8f" % (time.time()-start_time))
-    return frame
+    outputs = predictor(frame)
+    v = Visualizer(frame, MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+
+    return out.get_image()
+
+
+
+def main():
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        print()
+
+        ret, frame = cap.read()
+        orig_shape = frame.shape
+
+        start_time = time.time()
+
+        frame = process_frame(frame)
+        
+        if not ret:
+      	    print("webcam failed")
+      	    break
+
+        print("Processing time in sec:", "%.8f" % (time.time()-start_time))
+
+        frame = cv2.resize(frame, (orig_shape[1]*3, orig_shape[0]*3))
+        cv2.imshow("Webcam", frame)
+        if cv2.waitKey(1) & 0xFF == 27: # use ESC to quit
+            break
+
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 
 
 if __name__ == "__main__":
-
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        ret, frame = cap.read()
-
-
-        processed_frame = process_frame(frame)
-
-        cv2.imshow("Webcam", processed_frame)
-        
-        if cv2.waitKey(1) & 0xFF == 27: # use ESC to quit
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+    main()
